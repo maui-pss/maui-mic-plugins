f52ba17dbcefbdc2537b9847c929ab6b73691576
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2014-06-15 19:12:13 +0000

    livecd.py: Don't need to explicitely include ext3

--- mauimic/imager/livecd.py
+++ mauimic/imager/livecd.py
@@ -207,7 +207,7 @@ class LiveImageCreatorBase(LoopImageCreator):
         return env
 
     def __extra_filesystems(self):
-        return "vfat msdos isofs ext3 ext4 xfs btrfs"
+        return "vfat msdos isofs ext4 xfs btrfs"
 
     def __extra_drivers(self):
         retval = "sr_mod sd_mod ide-cd cdrom "
