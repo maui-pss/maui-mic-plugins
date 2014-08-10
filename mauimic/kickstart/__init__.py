cb9c07e9df401149dcdb7c860488910f30c18d36
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2014-08-10 05:26:47 +0000

    kickstart: Fix systemd unit path
    It's under /usr now.

--- mauimic/kickstart/__init__.py
+++ mauimic/kickstart/__init__.py
@@ -393,7 +393,7 @@ class XConfig(KickstartConfig):
             default_target = self.path('/etc/systemd/system/default.target')
             if os.path.islink(default_target):
                  os.unlink(default_target)
-            os.symlink('/lib/systemd/system/graphical.target', default_target)
+            os.symlink('/usr/lib/systemd/system/graphical.target', default_target)
 
 class DesktopConfig(KickstartConfig):
     """A class to apply a kickstart desktop configuration to a system."""
